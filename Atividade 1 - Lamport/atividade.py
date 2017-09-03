96bfafb90af6a6c7762137b313a7e776049c2bd7
Gabriela Ramos <gabrielaRamos>
2017-09-03 14:08:13 -0300

    Menu-testando Python

--- Atividade 1 - Lamport/atividade.py
+++ Atividade 1 - Lamport/atividade.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 
 import threading
-gitimport argparse
+import sys
+#import argparse
+
+def enviar():
+    print ("enviando")
+def receber():
+    print ("recebido")
+
+def menu():
+    print ("Selecione a opçao")
+    print ("1. Enviar mensagem")
+    print ("2. Visualizar mensagens recebidas")
+    print ("0. Sair")
+    opcao = input("Opção: ")
+
+    if opcao == '1':
+        enviar()
+    elif opcao == '2':
+        receber()
+    elif opcao == '0':
+        sys.exit()
 def main():
     # my code here
-    print("teste")
+    menu()
 
 if __name__ == "__main__":
     main()
