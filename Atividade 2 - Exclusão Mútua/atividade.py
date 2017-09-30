1678448b6521fbd33d4e9a76a618b58cb65441d5
Gabriela Ramos <gabrielaRamos>
2017-09-30 19:50:26 -0300

    Falta pouco

--- Atividade 2 - Exclusão Mútua/atividade.py
+++ Atividade 2 - Exclusão Mútua/atividade.py
@@ -1,3 +1,4 @@
+
 import sys
 from operator import itemgetter, attrgetter, methodcaller
 from threading import Thread, Lock
@@ -15,82 +16,92 @@ n_processo = 0
 intencao = 0
 obtive = 0
 time_intencao = 0
-lock = Lock()
+lock_a = Lock()
+lock_t = Lock()
+lock_i = Lock()
 oks = 0
+u_print = False
+leu = False
+
 class Exclusao(Thread): # Consome as mensagens recebidas
     def __init__(self ):
         Thread.__init__(self)
 
-
     def run(self):
-        global lock
+        global lock_t
         global oks
+        global time_intencao
+        global fila_app
+        global n_processo
+        global u_print
         solicitacao = list()
+        while 1+1 == 2 :
+            time.sleep(0.05)
+            lock_t.acquire()
+            if (len(fila_app) > 0):
+                print("INDO PROCESSAR UMA MENSAGEM")
+                solicitacao = fila_app.pop(0)
+                if ((solicitacao[1]) == 'q'): # Se alguém pediu o recurso
+                    if (intencao == 0): # E o recurso Não me interessa...
+                        e = Enviar(solicitacao[0][1], 0, cont, 'o') # enviar OK
+                        e.start()
+                    else: # Caso contrário, se me interessa o recurso
+                        print("Recebi uma proposta, mas meu time_intencao é:",time_intencao," e o do cara é:",str(solicitacao[0][0]), "Meu número é: ",n_processo," E o dele..: ",str(solicitacao[0][1]))
+                        if((time_intencao < (int)(solicitacao[0][0])) or ((time_intencao == (int)(solicitacao[0][0]))
+                         and n_processo <= (int)(solicitacao[0][1]))):
+                            if(n_processo != (int)(solicitacao[0][1])):
+                                fila_intencao.append(solicitacao[0][1]) #ganhei a disputa, adicionei o perdedor na fila
+                            if(n_processo == (int)(solicitacao[0][1])):
+                                oks = oks + 1;
+                                print("Estou somando meu ok", oks)
+                                if(oks == total_processos):
+                                    ticktacker = TickTacker()
+                                    ticktacker.start()
+                        else: #Perdi a disputa
+                            print("Perdi a disputa")
+                            u_print = False
+                            e = Enviar(solicitacao[0][1], 0, cont, 'o') # enviar OK
+                            e.start() # e chora
 
+                elif(solicitacao[1] == 'o'):
 
-        if (len(fila_app) > 0):
-            solicitacao = fila_app.pop(0)
-
-            if ((int)(solicitacao[1]) == 1): # Se alguém pediu o recurso
-
-                if (intencao == 0): # E o recurso Não me interessa...
-                    e = Enviar(solicitacao[0][1], 0, cont, 2) # enviar OK
-                    e.start()
-
-                else: # Caso contrário, se me interessa o recurso
-                    if((time_intencao <= (int)(solicitacao[0][0])) or ((time_intencao == (int)(solicitacao[0][0])) and n_processo < (int)(solicitacao[0][1]))):
-                        fila_intencao.append(solicitacao[0][1]) #ganhei a disputa, adicionei o perdedor na fila
-
-                        if(n_processo == (int)(solicitacao[0][1])):
-                            oks = oks + 1;
-
-                            if(oks == total_processos):
-                                ticktacker = TickTacker()
-                                ticktacker.start()
-                    else: #Perdi a disputa
-                        print("Perdi a disputa")
-                        e = Enviar(solicitacao[0][1], 0, cont, 2) # enviar OK
-                        e.start() # e chora
-                        oks = 0
-            else: #Se alguem enviou OK, preciso ver se esse OK é para mim.
-
-                if(n_processo == solicitacao[0][1]):
-
-                    oks = oks + 1;
-                    if(oks == total_processos):
-                        ticktacker = TickTacker()
-                        ticktacker.start()
-
-
-
-
+                    if(n_processo == int(solicitacao[0][1])):
+                        oks = oks + 1;
+                        print("oks", oks)
+                        if(oks == total_processos):
+                            ticktacker = TickTacker()
+                            ticktacker.start()
+            lock_t.release()
 
 class TickTacker(Thread):
     def __init__(self):
         Thread.__init__(self)
-        self.timer = random.randrange(2,5)
+        self.timer = random.randrange(5,8)
     def run(self):
         global intencao
         global oks
-        global lock
-        if(lock.acquire(True,5)):
-            print("CONSEGUI A LOCK")
-        else:
-            print("Como nissin?")
+        global lock_t
+        global u_print
+        lock_t.acquire()
+        if(u_print == True):
+            print("\n\n")
+            u_print = False
         print("Segurando o recurso! Contagem regressiva: ", end=" ")
         while(self.timer>0): #consumindo recurso
             sys.stdout.flush()
             print( self.timer, end = " ")
             self.timer = self.timer - 1
             time.sleep(1)
-        lock.release()
+        print("",flush=True)
+        print("Liberei recurso")
+        lock_t.release()
+
         oks = 0
         intencao = 0
         while(len(fila_intencao) > 0): #passar o recurso adiante
             pid = fila_intencao.pop(0)
-            e = Enviar(pid, 0, cont, 2)
+            e = Enviar(pid, 0, cont, 'o')
             e.start()
-            print("tamanho da fila", len(fila_intencao))
 
 class Mensagem():
     def __init__(self, msg, cont_acks, pid, cont, solicitaMsg):
@@ -107,27 +118,32 @@ class Mensagem():
     def tryAdd(self):
         global total_processos
         global fila_app
+        global lock_a
 
         if(self.msg == True and self.acks == total_processos):
             mid_solicitaMsg = list()
             mid_solicitaMsg.append(self.mid)
             mid_solicitaMsg.append(self.solicitaMsg)
+            # while(not lock_a.acquire()):
+                # time.sleep(0.05)
+            print("Estou adicionando na fila app")
             fila_app.append(mid_solicitaMsg)
-            e = Exclusao()
-            e.start()
+            # lock_a.release()
             return 1
 
 class Mensagens():
     def __init__(self):
         self.msg = list() #lista de Mensagens.
 
-    def insereOrdenado(self, ack, pid, cont, solicitaMsg):
+    def insereOrdenado(self, pid, ack, cont, solicitaMsg):
+        global fila_app
         if not self.msg: #se o vetor de mensagens é vazio
             if(ack == 1):
                 mensagem = Mensagem( False, ack, pid, cont, solicitaMsg)
 
             else:
                 mensagem = Mensagem( True, ack, pid, cont, solicitaMsg)
+
             self.msg.append(mensagem)
 
         else: # se o vetor não é vazio, insere o ack/mensagem nele
@@ -137,7 +153,7 @@ class Mensagens():
             mid.append(pid)
             flag = 0
             cnt = 0
-            global fila_app
+
             while(cnt < len(self.msg)):
                 if(self.msg[cnt].mid == mid):
                     flag = True #se mensagem ja é existente
@@ -185,46 +201,58 @@ class Receber(Thread):
 
             try:
                     serverSocket.bind(('',serverPort))
-                    serverSocket.listen(1)
+                    serverSocket.listen(100)
                     print ('*** Subindo o Processo de número ', self.num,' no total de ',total_processos,'***')
                     print('*** No ar através da porta: ', serverPort,' ***' )
 
                     while 1:
                         connectionSocket, addr = serverSocket.accept()
-
-                        try:
-                            msg = connectionSocket.recv(64)
-                            msg = msg.decode('utf-8') # "pid ack cont"
-                            vet = msg.split() # (pid, ack, cont)
-
-                            if(vet[3] == '2' and int(vet[0]) == n_processo):
-                                oks = oks + 1
-                                print("oks", oks)
-                                if(oks == int(total_processos)):
-                                    print("UHUL GANHEI ")
-                                    ticktacker = TickTacker()
-                                    ticktacker.start()
-
-                            if(vet[1] == '1'):
-                                # print ("Recebi ack mensagem da mensagem: ", vet[2], " ",vet[0],"da máquina: ", addr)
-                                mensagens.insereOrdenado(vet[1], vet[0], vet[2], vet[3])
-
-                            else:
-                                # print ("Recebi a mensagem: ", vet[2] ," ", vet[0] ," da máquina: ", addr)
-                                e = Enviar(vet[0], 1, vet[2], 0) # (pid ack cont)
-                                e.start()
-                                # print ("Enviando ack para a mensagem: ", vet[2] ," ", vet[0])
-                                mensagens.insereOrdenado(vet[1], vet[0], vet[2], vet[3])
-                                cont = (max(int(vet[2]), cont) + 1)
-
-                        except Exception as e :
-                            exec_type, exec_obj, exec_tb = sys.exc_info()
-                            print ("Erro!!!", exec_type, exec_tb.tb_lineno,"\n",e)
-                            sys.exit(2)
+                        TratarCliente(connectionSocket, addr).start()
 
             except Exception as e :
                     print (e)
                     os._exit(1)
+
+class TratarCliente(Thread):
+        def __init__ (self, connectionSocket, addr):
+              Thread.__init__(self)
+              self.addr = addr
+              self.connection = connectionSocket
+
+        def run(self):
+
+            global total_processos
+            global n_processo
+            global cont
+            global oks
+            global lock_i
+            try:
+                msg = self.connection.recv(64)
+                msg = msg.decode('utf-8') # "pid ack cont"
+                vet = msg.split() # (pid, ack, cont)
+                if(vet[1]=='0'):
+                    print("Recebi: ", msg)
+
+                if(vet[1] == '1'):
+                    # print ("Recebi ack mensagem da mensagem: ", vet[2], " ",vet[0],"da máquina: ", addr)
+
+                    lock_i.acquire()
+                    mensagens.insereOrdenado(vet[0], vet[1], vet[2], vet[3])
+                    lock_i.release()
+
+                else:
+                    e = Enviar(vet[0], 1, vet[2], '0') # (pid ack cont)
+                    e.start()
+                    # print ("Enviando ack para a mensagem: ", vet[2] ," ", vet[0])
+                    lock_i.acquire()
+                    mensagens.insereOrdenado(vet[0], vet[1], vet[2], vet[3])
+                    lock_i.release()
+                    cont = (max(int(vet[2]), cont) + 1)
+
+            except Exception as e :
+                exec_type, exec_obj, exec_tb = sys.exc_info()
+                print ("Erro!!!", exec_type, exec_tb.tb_lineno,"\n",e)
+                sys.exit(2)
 
 
 
@@ -240,21 +268,39 @@ class Enviar(Thread):
 
         def run(self):
             global cont
-
+            if(int(self.ack) != 1):
+                print("Enviei: [",self.pid,",",self.solicitaMsg,"]")
             for self.n in range (1, self.total+1):
 
                 try:
                     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    server_address = ('localhost', 12000 + self.n)
-                    sock.connect(server_address)
+                    sock.connect(('localhost', 12000 + int(self.n)))
                     msg = str(self.pid) + ' ' + str(self.ack) + ' ' + str(self.cont) + ' ' + str(self.solicitaMsg)
                     sock.send(msg.encode())
-                    if not self.ack:
-                        print ("Enviei a mensagem: ", self.cont ," " ,self.pid , " para ",self.n)
+                except:
+                        print("Falha ao enviar uma mensagem para o processo ", self.pid)
 
-                except Exception as e:
-                    print(e)
+class verifica(Thread):
+    def __init__ (self):
+          Thread.__init__(self)
 
+    def run(self):
+        global leu
+        global u_print
+        global lock_t
+        while 1:
+            timer = 1
+            while(timer > 0):
+                time.sleep(0.1)
+                timer = timer - 0.1
+            if(leu != True and u_print!= True and lock_t.acquire()):
+
+                print ("\nSelecione a opçao:")
+                print ("1. Solicitar recurso")
+                print ("0. Sair")
+                print ("Opção", end = ":", flush=True)
+                u_print = True
+                lock_t.release()
 def menu():
     global n_processo
     global total_processos
@@ -263,34 +309,40 @@ def menu():
     global intencao
     global cont
     global time_intencao
-    global lock
-
+    global lock_t
+    global u_print
+    global leu
     mensagens = Mensagens()
-    while 1:
-        lock.acquire()
-        print("Travei a trava")
-        print("\n\n")
-        print ("Selecione a opçao:")
-        print ("1. Solicitar recurso")
-        print ("0. Sair")
 
-        opcao = input("Opção: ")
-        lock.release()
+    while 1:
+        time.sleep(0.5)
+        if(lock_t.acquire()):
+            print ("Selecione a opçao:")
+            print ("1. Solicitar recurso")
+            print ("0. Sair")
+            u_print = True
+            lock_t.release()
+            leu = False
+            opcao = input("Opção: ")
+            leu = True
+
         print()
         if opcao == '1':
+            print("Aguardando por OKs")
             intencao = 1
-            enviar = Enviar(n_processo, 0, cont, 1) #pid ack cont
+            oks = 0;
+            time_intencao = cont
+            enviar = Enviar(n_processo, 0, cont, 'q') #pid ack cont
             enviar.start()
             time.sleep(0.05)
 
-            time_intencao = cont
-            oks = 0;
         elif opcao == '0':
             print("\n\nAdeus amiguinho!")
             os._exit(0)
         else:
             print("\n\n\nOpção Inválida!!!\n")
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
     if( len(sys.argv)!=3):
         print("Chamada inválida use: $ python3 atividade.py NUM_PROCESSO TOTAL_PROCESSOS")
         sys.exit(1)
@@ -301,7 +353,11 @@ def main():
     total_processos = int(sys.argv[2])
     a = Receber(n_processo)
     a.start()
+    e = Exclusao()
+    e.start()
     time.sleep(0.09)
+    v = verifica()
+    v.start()
     menu()
 
 if __name__ == "__main__":
